027fc24f6c01b213ba26d8768a32bd3a67f78c08
Ayeris <amaury.delprat@epita.fr>
2019-03-15 13:01:18 +0000

    ok

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     static void print (String S)
     {
@@ -42,6 +45,17 @@ public class Main {
     public static void main(String[] args)
     {
         String first = "A young prince was coming of age. As part of the kingdoms traditions, he had to prove himself as worthy heir to the throne by performing a dangerous mission. His mission is to journey to the wastelands beyond the kingdom perimeter and find treasures worthy to present to the King, his farther.";
+        String sec = "Hamza the only prince of the Kingdom of Tanélia is a prince loved by everyone.\\n\\nA legend is told in his kingdom, the legendof the diamond heart who’s the biggest diamond of the kingdom.This diamond is supposed to be in a donjon guarded by horriblemonsters. To reach the donjon you must cross a malefic forest infested of monster. \\n\\nThis prince dream about this treasure and train with the best knights of the kingdom to get strong enough to face all the dangers. The only problem is that the kingand the queen don’t want to let him leave so he must escape from the castle with the help of his favorite maid.";
+
         Prince Hamza = new Prince();
+        List<Character> party = new ArrayList<Character>();
+        party.add(Hamza);
+
+        List<String> choices = new ArrayList<String>();
+        choices.add("say goodbye");
+        choices.add("Turn around without a goodbye");
+
+        Encounter castleE = new Encounter(first, choices);
+        Room castle = new Room("Castle", "None", "None", castleE, party);
     }
 }
--- src/Prince.java
+++ src/Prince.java
@@ -1,7 +1,10 @@
 public class Prince extends Character
 {
+    int gold;
+
     public Prince ()
     {
         name = "Hamza";
+        gold = 2147483647;
     }
 }
